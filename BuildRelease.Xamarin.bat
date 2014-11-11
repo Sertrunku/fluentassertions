f2165f517175a076ee230157243f281be9e1360f
Oren Novotny <oren@novotny.org>
2014-11-10 23:57:20 -0500

    fix path

--- BuildRelease.Xamarin.bat
+++ BuildRelease.Xamarin.bat
@@ -2,6 +2,6 @@ CALL "%VS120COMNTOOLS%vsvars32.bat"
 
 msbuild /p:Configuration=Release /p:Platform="Any CPU" /t:Rebuild FluentAssertions.Xamarin.sln
 
-tools\GitLink.exe .\lib -u https://github.com/onovotny/fluentassertions -b Xamarin-Support
+tools\GitLink.exe . -u https://github.com/onovotny/fluentassertions -b Xamarin-Support
 
 tools\nuget pack package\FluentAssertions.Xamarin.nuspec -o package
