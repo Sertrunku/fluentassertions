2028f581bf679ca1e45d8a3625338011737414c9
Oren Novotny <oren@novotny.org>
2015-12-31 20:44:20 -0500

    Update build script

--- BuildRelease.Xamarin.bat
+++ BuildRelease.Xamarin.bat
@@ -1,8 +1,10 @@
 CALL "%VS140COMNTOOLS%vsvars32.bat"
 
+
+.nuget\nuget install gitlink -SolutionDir . -Verbosity quiet -ExcludeVersion -pre
 .nuget\nuget restore FluentAssertions.Xamarin.sln
 msbuild /p:Configuration=Release /p:Platform="Any CPU" /t:Rebuild FluentAssertions.Xamarin.sln
 
-tools\GitLink.exe . -u https://github.com/onovotny/fluentassertions -b Xamarin-Support
+packages\gitlink\lib\net45\GitLink.exe . -u https://github.com/onovotny/fluentassertions -b Xamarin-Support -errorsaswarnings
 
-.nuget\nuget pack package\FluentAssertions.Xamarin.nuspec -o package
+.nuget\nuget pack src\package\FluentAssertions.Xamarin.nuspec 
